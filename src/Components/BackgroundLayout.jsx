cd54f52ea0f49e77455af18361b88b9425596312
Sarim Khan <sarim.khan0505@gmail.com>
2024-02-25 12:44:07 +0530

    images shifted to cloudinary

--- src/Components/BackgroundLayout.jsx
+++ src/Components/BackgroundLayout.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useStateContext } from '../Context'
 //images
-import Clear from '../assets/images/Clear.jpg'
-import Fog from '../assets/images/fog.png'
-import Cloudy from '../assets/images/Cloudy.jpg'
-import Rainy from '../assets/images/Rainy.jpg'
-import Snow from '../assets/images/snow.jpg'
-import Stormy from '../assets/images/Stormy.jpg'
-import Sunny from '../assets/images/Sunny.jpg'
+const Snow = "https://res.cloudinary.com/dwvd0f79c/image/upload/v1708843834/weather%20task/yyhaftj7q0tvhspcp1ba.jpg"
+const Stormy = "https://res.cloudinary.com/dwvd0f79c/image/upload/v1708843834/weather%20task/xmuyzvuls2ibsyyexuw5.jpg"
+const Fog = "https://res.cloudinary.com/dwvd0f79c/image/upload/v1708843833/weather%20task/mlbq3gcrwef6qmudouq7.jpg"
+const Cloudy = "https://res.cloudinary.com/dwvd0f79c/image/upload/v1708843834/weather%20task/lyhrfiq6kg4bjm4u0qlz.jpg"
+const Rainy = "https://res.cloudinary.com/dwvd0f79c/image/upload/v1708843835/weather%20task/hlijbma4wwab9bzg5vxr.jpg"
+const Clear = "https://res.cloudinary.com/dwvd0f79c/image/upload/v1708843691/weather%20task/jsma8gzuwvarmbji2gtj.jpg"
+const Sunny = "https://res.cloudinary.com/dwvd0f79c/image/upload/v1708843836/weather%20task/bfcepv9plyfdetji8vk5.jpg"
 
 const BackgroundLayout = () => {
 
@@ -34,6 +34,7 @@ const BackgroundLayout = () => {
   }, [weather])
 
   return (
+
     <img src={image} alt="weather_image" className='h-screen w-full fixed left-0 top-0 -z-[10]' />
   )
 }
